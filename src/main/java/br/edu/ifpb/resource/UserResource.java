7651d499113613b291b2ef024bd03f23843bb351
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-10-09 14:26:39 -0300

    Adicionado classe de configuração e integração do Thymeleaf no Spring Boot(Spring Boot já faz isso, classe foi feita apenas para ter noção de como funciona "por de baixo dos panos")! UserResouce agora é um Controller ao invés de um RestController!

--- src/main/java/br/edu/ifpb/resource/UserResource.java
+++ src/main/java/br/edu/ifpb/resource/UserResource.java
@@ -4,16 +4,16 @@ import br.edu.ifpb.domain.User;
 import br.edu.ifpb.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@RestController
+@Controller
 public class UserResource {
 
     @Autowired
